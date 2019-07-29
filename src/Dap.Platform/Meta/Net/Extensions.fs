2c64571358c3698caef35c351cdcc3912a04b2b3
YJ Park <yjpark@gmail.com>
2019-07-29 19:09:40 +0800

    M.instant and M.duration with format seems not working

--- src/Dap.Platform/Meta/Net/Extensions.fs
+++ src/Dap.Platform/Meta/Net/Extensions.fs
@@ -8,6 +8,7 @@ open Dap.Platform
 open Dap.Platform.Meta
 
 type M with
+    //TODO: this is not working yet
     static member instant (format : InstantFormat, key, ?value : string, ?validator : string) =
         let value = value |> Option.defaultValue "(getNow' ())"
         match format with
@@ -17,14 +18,17 @@ type M with
             sprintf "InstantFormat.%s" <| Union.getKind format
         |> fun t -> FieldMeta.CreateCustom t key value validator
         |> fun m -> m.ToAlias "Instant"
+    //TODO: this is not working yet
     static member instant (key, v : Instant, ?validator : string) =
         let v = InstantFormat.General.Format v
         M.instant (InstantFormat.General, key, value = v, ?validator = validator)
 
 type M with
+    //TODO: this is not working yet
     static member duration (key, value : Duration, ?validator : string) =
         M.duration (key, "", ?validator = validator)
         |> fun m -> m.WithValue (E.duration value)
+    //TODO: this is not working yet
     static member duration (format : DurationFormat, key, ?value : string, ?validator : string) =
         let value = value |> Option.defaultValue "noDuration"
         match format with
@@ -34,6 +38,7 @@ type M with
             sprintf "DurationFormat.%s" <| Union.getKind format
         |> fun t -> FieldMeta.CreateCustom t key value validator
         |> fun m -> m.ToAlias "Duration"
+    //TODO: this is not working yet
     static member duration (format : DurationFormat, key, value : Duration, ?validator : string) =
         M.duration (format, key, "", ?validator = validator)
         |> fun m -> m.WithValue (format.JsonEncoder value)
